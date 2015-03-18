96e8f7b4fe54de90d83e1c24d119d7914ab4e3a1
Brian Hrolenok <brian.hrolenok@gmail.com>
2015-03-18 14:47:22 -0400

    Oh man it works
    I think it works...
    Two changes:
    1) DaD now accumulates new instances instead of only
       adding on to the set of training instances
    2) Withholds 20% of the data as CV data, and prints the CV error
       after each training iteration.
    3) Now saves the set of models as a pickled list in
       "dad-results.p" in the current directory

--- dad.py
+++ dad.py
@@ -1,4 +1,4 @@
-import numpy, btfutil, scipy.spatial, subprocess, time, sys
+import numpy, btfutil, scipy.spatial, subprocess, time, sys, cPickle
 
 class KNN():
 	def __init__(self,features,ys):
@@ -24,10 +24,12 @@ def predictLR(model,num_steps,initialPlacementBTFDir):
 	rv.filter_by_col('dbool')
 	return rv
 
-def learnLR(features,ys):
+def learnLR(features,ys,cv_features=None,cv_ys=None):
 	result = numpy.linalg.lstsq(features,ys)
-	print "Residuals:",result[1]
+	if not((cv_features is None) or (cv_ys is None)):
+		print "CV error:",numpy.linalg.norm(cv_ys - (cv_features.dot(result[0])))
 	return result[0]
+
 def learnKNN(features,ys):
 	return KNN(features,ys)
 
@@ -46,21 +48,30 @@ def dad(N,k,training_dir,learn,predict,feature_names = ['rbfsepvec','rbforivec',
 	btf = btfutil.BTF()
 	btf.import_from_dir(training_dir)
 	btf.filter_by_col('dbool')
-	training_features, training_ys = btf2data(btf,feature_names)
-	training_trajectory = split_btf_trajectory(btf,['xpos','ypos','timage'])
+	features, ys = btf2data(btf,feature_names)
+	tmpidx = int(features.shape[0]*0.8)
+	training_features = features[:tmpidx,:]
+	cv_features = features[tmpidx:,:]
+	training_ys = ys[:tmpidx,:]
+	cv_ys = ys[tmpidx:,:]
+	trajectory = split_btf_trajectory(btf,['xpos','ypos','timage'])
+	training_trajectory = {eyed:trajectory[eyed][:tmpidx,:] for eyed in trajectory}
+	cv_trajectory = {eyed:trajectory[eyed][tmpidx:,:] for eyed in trajectory}
 	# min_seq_length = min(map(lambda key: training_trajectory[key].shape[0],training_trajectory)) 
 	# predict_steps = min_seq_length
 	# if k > 0:
 	# 	predict_steps = min(min_seq_length,k)
 	# print "k=",predict_steps
-	training_traj_features = split_btf_trajectory(btf,feature_names)
+	# training_traj_features = split_btf_trajectory(btf,feature_names)
 	models = (learn(training_features,training_ys),)
+	dad_training_features, dad_training_ys = None, None
 	for n in range(N):
 		sim_btf = predict(models[n],k,training_dir)
 		sim_features,sim_ys = btf2data(sim_btf,feature_names)
 		sim_trajectory = split_btf_trajectory(sim_btf,['xpos','ypos','timage'])
 		sim_traj_features = split_btf_trajectory(sim_btf,feature_names)
-		dad_training_features, dad_training_ys = training_features, training_ys
+		if dad_training_features is None:
+			dad_training_features, dad_training_ys = training_features, training_ys
 		for eyed in sim_trajectory:
 			traj = sim_trajectory[eyed]
 			traj_feats = sim_traj_features[eyed]
@@ -69,12 +80,13 @@ def dad(N,k,training_dir,learn,predict,feature_names = ['rbfsepvec','rbforivec',
 				dad_sample_ys = training_trajectory[eyed][row_idx+1]-traj[row_idx]
 				dad_training_features = numpy.row_stack([dad_training_features,dad_sample_feats])
 				dad_training_ys = numpy.row_stack([dad_training_ys, dad_sample_ys])
-		models = models + (learn(dad_training_features,dad_training_ys),)
+		models = models + (learn(dad_training_features,dad_training_ys,cv_features,cv_ys),)
 	return models
 
 def main(training_dir,num_models,max_seq_len):
 	models = dad(num_models,max_seq_len,training_dir,learnLR,predictLR)
 	print models
+	cPickle.dump(models,open("dad-results.p","w"))
 
 if __name__ == '__main__':
 	main(sys.argv[1],int(sys.argv[2]),int(sys.argv[3]))
