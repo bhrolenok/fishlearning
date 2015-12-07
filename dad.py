c5a87bd4a48c9edd049d2f5bf38815581cf46558
Brian Hrolenok <brian.hrolenok@gmail.com>
2015-12-07 18:42:04 -0500

    Random Fourier Features, Initial Commit (UNTESTED)
    generate_feature_map(n_f,D)
    - returns a function that maps an array of features (rows = samples,
      cols = features) into a randomly sampled subspace of the fourier
      transform. Function object will have parameterization of sampling
      attached as 'ws' and 'bs'. In order to use with DaD, need to
      modify FishLR to use transformed features.

--- dad.py
+++ dad.py
@@ -49,6 +49,16 @@ def learnLR_regularized(features,ys,cv_features=None,cv_ys=None, lamb=0.0):
 		print "CV error:",numpy.linalg.norm(cv_ys - (cv_features.dot(result[0])))
 	return result[0]
 
+def generate_feature_map(n_f,D):
+	# n_f = features.shape[1]
+	ws = numpy.random.multivariate_normal(mean=numpy.zeros(n_f),cov=numpy.eye(n_f),size=D)
+	bs = numpy.random.random(size=n_f)
+	def feature_map(feats):
+		return numpy.cos((ws.dot(feats)+numpy.tile(bs,(1,feats.shape[0]))).T)
+	feature_map.ws = ws
+	feature_map.bs = bs
+	return feature_map
+
 def learnKNN(features,ys):
 	return KNN(features,ys)
 
