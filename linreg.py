891ebac5fdc62c09981df2062c1b93a42bbbdb3b
Brian Hrolenok <brian.hrolenok@gmail.com>
2016-03-31 12:45:36 -0400

    singleAgent and allAgents
    singleAgent now chooses a single ID at random from the set, and
    only simulates one. Using all agents caused learned behavior to
    be worse than before. allAgents now works as singleAgent did in
    previous commit.

--- linreg.py
+++ linreg.py
@@ -48,31 +48,29 @@ def learnLR(features,ys,cv_features=None,cv_ys=None,feature_column_names=None):
 	return result[0]
 
 def predictLR_allAgents(model,num_steps,initialPlacementBTF,logdir=None):
-	if logdir is None:
-		logdir = os.getcwd()
+	# if logdir is None:
+	# 	logdir = os.getcwd()
 	
-	outname = os.path.join(logdir,'lr_coeff.txt')
-	prefix = "[BTFLogger] Starting new logs in"
-	outf = open(outname,'w')
-	for row in model:
-		outf.write("%f %f %f\n"%(row[0],row[1],row[2]))
-	outf.close()
-	outf = open(os.path.join(logdir,"initial_placement.txt"),"w")
-	btfutil.writeInitialPlacement(outf,initialPlacementBTF)
-	outf.close()
-	# WARNING! BELOW USES OLD FishLR NOT FishReynolds!
-	# proc = subprocess.Popen(['java','biosim.app.fishlr.FishLR','-placed','-btf',initialPlacementBTFDir,'-nogui','-logging', '-lr', outname,'-for',str(num_steps)],stdout=subprocess.PIPE)
-	proc = subprocess.Popen(['java','biosim.app.fishreynolds.FishReynolds','-placed', os.path.join(logdir,'initial_placement.txt'),'-nogui','-logging', logdir, '-lr', outname,'-for',str(num_steps)],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-	output,errors = proc.communicate()
-	trace_btfdir_start = len(prefix)+output.index(prefix)
-	trace_btfdir_end = output.index("\n",trace_btfdir_start)
-	trace_btfdir = output[trace_btfdir_start:trace_btfdir_end].strip()
-	rv = btfutil.BTF()
-	rv.import_from_dir(trace_btfdir)
-	rv.filter_by_col('dbool')
-	return rv
-
-def predictLR_singleAgent(model,num_steps,initialPlacementBTF,logdir=None):
+	# outname = os.path.join(logdir,'lr_coeff.txt')
+	# prefix = "[BTFLogger] Starting new logs in"
+	# outf = open(outname,'w')
+	# for row in model:
+	# 	outf.write("%f %f %f\n"%(row[0],row[1],row[2]))
+	# outf.close()
+	# outf = open(os.path.join(logdir,"initial_placement.txt"),"w")
+	# btfutil.writeInitialPlacement(outf,initialPlacementBTF)
+	# outf.close()
+	# # WARNING! BELOW USES OLD FishLR NOT FishReynolds!
+	# # proc = subprocess.Popen(['java','biosim.app.fishlr.FishLR','-placed','-btf',initialPlacementBTFDir,'-nogui','-logging', '-lr', outname,'-for',str(num_steps)],stdout=subprocess.PIPE)
+	# proc = subprocess.Popen(['java','biosim.app.fishreynolds.FishReynolds','-placed', os.path.join(logdir,'initial_placement.txt'),'-nogui','-logging', logdir, '-lr', outname,'-for',str(num_steps)],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+	# output,errors = proc.communicate()
+	# trace_btfdir_start = len(prefix)+output.index(prefix)
+	# trace_btfdir_end = output.index("\n",trace_btfdir_start)
+	# trace_btfdir = output[trace_btfdir_start:trace_btfdir_end].strip()
+	# rv = btfutil.BTF()
+	# rv.import_from_dir(trace_btfdir)
+	# rv.filter_by_col('dbool')
+	# return rv
 	if logdir is None:
 		logdir = os.getcwd()
 	
@@ -119,6 +117,44 @@ def predictLR_singleAgent(model,num_steps,initialPlacementBTF,logdir=None):
 			tmpBtf.import_from_dir(trace_btfdir)
 			tmpBtf.filter_by_col('dbool')
 			rv = btfutil.merge_by_column(rv,tmpBtf,'clocktime')
+	return rv
+
+
+def predictLR_singleAgent(model,num_steps,initialPlacementBTF,logdir=None):
+	if logdir is None:
+		logdir = os.getcwd()
+	
+	outname = os.path.join(logdir,'lr_coeff.txt')
+	exampleBTFDir = os.path.join(logdir,'exampleBTF')
+	os.mkdir(exampleBTFDir)
+	prefix = "[BTFLogger] Starting new logs in"
+	outf = open(outname,'w')
+	for row in model:
+		outf.write("%f %f %f\n"%(row[0],row[1],row[2]))
+	outf.close()
+	outf = open(os.path.join(logdir,"initial_placement.txt"),"w")
+	numInitIds = btfutil.writeInitialPlacement(outf,initialPlacementBTF)
+	outf.close()
+	initialPlacementBTF.save_to_dir(exampleBTFDir)
+	# WARNING! BELOW USES OLD FishLR NOT FishReynolds
+	# proc = subprocess.Popen(['java','biosim.app.fishlr.FishLR','-placed','-btf',initialPlacementBTFDir,'-nogui','-logging', '-lr', outname,'-for',str(num_steps)],stdout=subprocess.PIPE)
+	rv = None
+	activeID = initialPlacementBTF['id'][numpy.random.randint(numInitIds)]
+	proc = subprocess.Popen(['java','biosim.app.fishreynolds.FishReynolds',\
+		'-placed', os.path.join(logdir,'initial_placement.txt'),\
+		'-nogui',\
+		'-logging', logdir, \
+		'-lr', outname,\
+		'-replay',exampleBTFDir,\
+		'-ignoreTrackIDs',initialPlacementBTF['id'][activeID],\
+		'-for',str(num_steps)],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+	output,errors = proc.communicate()
+	trace_btfdir_start = len(prefix)+output.index(prefix)
+	trace_btfdir_end = output.index("\n",trace_btfdir_start)
+	trace_btfdir = output[trace_btfdir_start:trace_btfdir_end].strip()
+	rv = btfutil.BTF()
+	rv.import_from_dir(trace_btfdir)
+	rv.filter_by_col('dbool')
 	return rv
 
 
