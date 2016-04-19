80cfab8cca17d51f1b46be14901c4c53530881e4
Brian Hrolenok <brian.hrolenok@gmail.com>
2016-04-19 16:45:57 -0400

    Forgot to write out timestamp file. Durrr.

--- parseFishAndFrames.py
+++ parseFishAndFrames.py
@@ -4,9 +4,9 @@ import numpy, scipy.io
 
 VERBOSE_TIMEOUT=30
 
-def main(matfile_fname,btf_outdir):
+def main(matfile_fname,btf_outdir,fps):
 	print "Saving BTF to dir [{}]".format(btf_outdir)
-	print "Columns: {}".format(['id','ximage','yimage','timage_x','timage_y','timage','timestamp'])
+	print "Columns: {}".format(['id','ximage','yimage','timage_x','timage_y','timage','timestamp','clocktime'])
 	id_btf = open(os.path.join(btf_outdir,'id.btf'),'w')
 	ximage_btf = open(os.path.join(btf_outdir,'ximage.btf'),'w')
 	yimage_btf = open(os.path.join(btf_outdir,'yimage.btf'),'w')
@@ -14,6 +14,7 @@ def main(matfile_fname,btf_outdir):
 	timage_y_btf = open(os.path.join(btf_outdir,'timage_y.btf'),'w')
 	timage_btf = open(os.path.join(btf_outdir,'timage.btf'),'w')
 	timestamp_btf = open(os.path.join(btf_outdir,'timestamp.btf'),'w')
+	clocktime_btf = open(os.path.join(btf_outdir,'clocktime.btf'),'w')
 	print "Loading from file [{}]".format(matfile_fname)
 	data = scipy.io.loadmat(matfile_fname)
 	print "done."
@@ -26,15 +27,18 @@ def main(matfile_fname,btf_outdir):
 	lastidx = 2
 	for frame_idx in range(2,num_frames-1):
 		for detection_idx in range(len(data['frames'][frame_idx]['onfish'][0][0])):
+			writeit(timestamp_btf,frame_idx)
+			clocktime = float(frame_idx)/float(fps)
+			writeit(clocktime_btf,clocktime)
 			fishid = data['frames'][frame_idx]['onfish'][0][0][detection_idx]
 			writeit(id_btf,fishid)
-			ximage = data['frames'][frame_idx]['px'][0][detection_idx]
+			ximage = data['frames'][frame_idx]['px'][0][detection_idx][0]
 			writeit(ximage_btf,ximage)
-			yimage = data['frames'][frame_idx]['py'][0][detection_idx]
+			yimage = data['frames'][frame_idx]['py'][0][detection_idx][0]
 			writeit(yimage_btf,yimage)
-			timage_x = data['frames'][frame_idx]['vx'][0][detection_idx]
+			timage_x = data['frames'][frame_idx]['vx'][0][detection_idx][0]
 			writeit(timage_x_btf,timage_x)
-			timage_y = data['frames'][frame_idx]['vy'][0][detection_idx]
+			timage_y = data['frames'][frame_idx]['vy'][0][detection_idx][0]
 			writeit(timage_y_btf,timage_y)
 			timage = numpy.arctan2(timage_y,timage_x)
 			writeit(timage_btf,timage)
@@ -45,4 +49,4 @@ def main(matfile_fname,btf_outdir):
 			lastidx = frame_idx
 
 if __name__ == '__main__':
-	main(sys.argv[1],sys.argv[2])
+	main(sys.argv[1],sys.argv[2],float(sys.argv[3]))
